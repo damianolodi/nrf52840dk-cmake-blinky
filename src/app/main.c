cc47f8d3037ee4f6b3fb50676dc25164d89bc689
Damiano <damiano.lodi.93@gmail.com>
2022-01-09 20:39:39 +0100

    Add srouce code project structure

---
+++ src/app/main.c
@@ -0,0 +1,32 @@
+/** @file
+ *
+ * @defgroup nRF52840_blinky main.c
+ * @{
+ * @ingroup blinky_example
+ * @brief Blinky Example Application main file.
+ *
+ * This file contains the source code for a sample application to blink LEDs.
+ *
+ */
+
+// #include <stdbool.h>
+// #include <stdint.h>
+
+#include "nrf_delay/nrf_delay.h"
+#include "nrfx.h"
+
+/**
+ * @brief Function for application main entry.
+ */
+int main(void) {
+    /* Configure board. */
+    // bsp_board_init(BSP_INIT_LEDS);
+
+    // /* Toggle LEDs. */
+    // while (true) {
+    //     for (int i = 0; i < LEDS_NUMBER; i++) {
+    //         bsp_board_led_invert(i);
+    //         nrf_delay_ms(500);
+    //     }
+    // }
+}
