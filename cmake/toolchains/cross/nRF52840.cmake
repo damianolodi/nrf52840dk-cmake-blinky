9f71b237267f0d51ba1ba3da9a77023f1f24754f
Damiano <damiano.lodi.93@gmail.com>
2022-01-18 19:37:42 +0100

    Move compilation and linker flags into toolchain file

--- cmake/toolchains/cross/nRF52840.cmake
+++ cmake/toolchains/cross/nRF52840.cmake
@@ -17,8 +17,17 @@ set(LINKER_SCRIPT_PRIMARY_FILE nrf52840_xxaa.ld)
 # AddExecutableWithLinkerScriptDep module
 list(APPEND LINKER_SCRIPT_DEPENDENCIES "${LINKER_SCRIPT_DIR}/${LINKER_SCRIPT_PRIMARY_FILE}")
 
-set(CPU_FLAGS "-D__HEAP_SIZE=8192 -D__STACK_SIZE=8192 -DNRF52840_XXAA -DBOARD_PCA10056")
+set(CPU_FLAGS "-DNRF52840_XXAA -DBOARD_PCA10056")
+set(CPU_FLAGS "${CPU_FLAGS} -D__HEAP_SIZE=8192 -D__STACK_SIZE=8192")
+set(CPU_FLAGS "${CPU_FLAGS} -mabi=aapcs")
+set(CPU_FLAGS "${CPU_FLAGS} -fno-builtin -fshort-enums")
+
 set(LD_FLAGS "-T${LINKER_SCRIPT_PRIMARY_FILE} -L${LINKER_SCRIPT_DIR}")
-#set(LD_FLAGS "--specs=nano.specs")
+set(LD_FLAGS "${LD_FLAGS} --specs=nano.specs")
+set(LD_FLAGS "${LD_FLAGS} -mthumb -mabi=aapcs -mcpu=cortex-m4")
+set(LD_FLAGS "${LD_FLAGS} -mfloat-abi=hard -mfpu=fpv4-sp-d16")
+set(LD_FLAGS "${LD_FLAGS} -lc -lnosys -lm")
+# Debug and optimization mode configuration will be added later
+set(LD_FLAGS "${LD_FLAGS} -O3 -g3")
 
 include(${CMAKE_CURRENT_LIST_DIR}/cortex-m4_hardfloat.cmake)
