ff4d9e182536121205063cb6e445996925ff0a74
Damiano <damiano.lodi.93@gmail.com>
2022-01-18 19:03:08 +0100

    Add source code comments

--- src/app/board_config.h
+++ src/app/board_config.h
@@ -1,4 +1,17 @@
+/**
+ * @file board_config.h
+ * @author Damiano Lodi
+ * @brief Peripherals configuraiton.
+ * @date 2022-01-18
+ *
+ * @copyright Copyright (c) 2022 Damiano Lodi. This project is released under
+ * the MIT public license.
+ */
+
 #include "nrf_gpio.h"
 #include "pca10056.h"
 
+/**
+ * @brief Configure board peripherals.
+ */
 void board_config() { nrf_gpio_pin_dir_set(LED_1, NRF_GPIO_PIN_DIR_OUTPUT); }
